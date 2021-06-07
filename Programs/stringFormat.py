0a8ad29747a025ff3b9d0630f02edabc64d092a3
gkumar@doxim.xom <gkumar@doxim.xom>
2021-06-08 00:34:50 +0530

    Adding more string practice codes

---
+++ Programs/stringFormat.py
@@ -0,0 +1,2 @@
+informatedString = "{0} is an aminal, {0} has four legs and two ears, {0} eats grass"
+print(informatedString.format("Cow"))
---
+++ Programs/stringModify.py
@@ -0,0 +1,6 @@
+a=" HelloIAmString "
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace("Hello","Replacement"))
+print(a.split("I"))
